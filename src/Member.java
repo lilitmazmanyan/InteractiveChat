e336ad27ba2ed3070a779057bebc96098c55b0cb
Lilit <lilit.mazmanyan1@gmail.com>
2019-12-22 21:39:41 +0400

    Remover redundant getter/setters from Member source file

--- src/Member.java
+++ src/Member.java
@@ -2,27 +2,7 @@ import java.util.Date;
 
 public class Member {
     String name;
-    String message;
-    Date sentMessageTime;
-
     public Member(String name) {
         this.name = name;
     }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public Date getSentMessageTime() {
-        return sentMessageTime;
-    }
-
-    public void setSentMessageTime(Date sentMessageTime) {
-        this.sentMessageTime = sentMessageTime;
-    }
-
 }
